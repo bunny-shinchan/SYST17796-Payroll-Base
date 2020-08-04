dd6e5586f93a855e5d6c78e59403a22703cca086
bunny-shinchan <65793304+bunny-shinchan@users.noreply.github.com>
2020-08-04 15:38:44 -0400

    Sudhanshu changed this

---
+++ src/payroll/PayrollSingle.java
@@ -0,0 +1,13 @@
+
+package payroll;
+
+/**
+ *
+ * @author vermsudh
+ */
+public class PayrollSingle {
+
+   public static void main(String[] args) {
+		PayrollFactory factory = PayrollFactory.getInstanc();//this will add singleton
+	} 
+}
